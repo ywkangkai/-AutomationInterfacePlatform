32bb8390c130ca6fe952e8f5f5b7250c6931e142
kangkai <709351930@qq.com>
2021-05-22 11:07:35 +0800

    增加setting注释

--- autoplatform/settings.py
+++ autoplatform/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+####asdsadasdsaasdasda
 ROOT_URLCONF = 'autoplatform.urls'
 
 TEMPLATES = [
