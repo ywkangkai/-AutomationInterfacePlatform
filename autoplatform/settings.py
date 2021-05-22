e751ae652b815b0f8f45b282aeb58efce0cb525a
kangkai <709351930@qq.com>
2021-05-22 11:48:25 +0800

    在setting中修改数据库为mysql

--- autoplatform/settings.py
+++ autoplatform/settings.py
@@ -73,12 +73,15 @@ WSGI_APPLICATION = 'autoplatform.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dev04',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 111111
     }
 }
 
