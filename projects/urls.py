273396e8d5dcb5a5fa440cd57661f7a1c3749f3b
kangkai <709351930@qq.com>
2021-06-13 14:08:20 +0800

    使用ViewSet简化增删查改

--- projects/urls.py
+++ projects/urls.py
@@ -3,9 +3,19 @@ from django.urls import path, re_path
 # from projects.views import
 from projects import views
 
-
+'''
+需要集成ViewSet才能使用这种写法，
+使用映射关系，根据不通的路径去找不通的方法
+'''
 urlpatterns = [
-    path('projects/', views.ProjectsView.as_view()),
-    path('projects/<int:pk>/', views.ProjectDetailView.as_view()),
+    path('projects/', views.ProjectViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    })),
+    path('projects/<int:pk>/', views.ProjectViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'
+    })),
 
 ]
