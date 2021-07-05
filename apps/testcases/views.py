97d8432f53ed89b74026fd18470c42318ecb8005
kangkai <709351930@qq.com>
2021-07-05 23:00:20 +0800

    编辑用例转化为前端是识别的格式

--- apps/testcases/views.py
+++ apps/testcases/views.py
@@ -3,9 +3,8 @@ import logging
 
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Count
-
-# from rest_framework import filters
-from rest_framework.filters import OrderingFilter
+from interfaces.models import Interfaces
+from utils import handle_datas
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework import permissions
@@ -26,9 +25,86 @@ class TestcasesViewSet(viewsets.ModelViewSet):
     serializer_class = TestcasesModelSerializer
 
     def retrieve(self, request, *args, **kwargs):
+        """获取用例详情信息"""
+        # Testcase对象
         testcase_obj = self.get_object()
-        #获取用例前置信息
-        testcase_include = json.loads(testcase_obj.include,encoding='utf-8')
-        print(testcase_include)
-        return testcase_include
 
+        # 用例前置信息
+        # testcase_include = eval(testcase_obj.include)
+        testcase_include = json.loads(testcase_obj.include, encoding='utf-8')
+
+        # 用例请求信息
+        # testcase_request = eval(testcase_obj.request)
+        testcase_request = json.loads(testcase_obj.request, encoding='utf-8')
+        testcase_request_datas = testcase_request.get('test').get('request')
+
+        # 处理用例的validate列表
+        # 将[{'check': 'status_code', 'expected':200, 'comparator': 'equals'}]
+        # 转化为[{key: 'status_code', value: 200, comparator: 'equals', param_type: 'string'}]
+        testcase_validate = testcase_request.get('test').get('validate')
+        testcase_validate_list = handle_datas.handle_data1(testcase_validate)
+
+        # 处理用例的param数据
+        testcase_params = testcase_request_datas.get('params')
+        testcase_params_list = handle_datas.handle_data4(testcase_params)
+
+        # 处理用例的header列表
+        testcase_headers = testcase_request_datas.get('headers')
+        testcase_headers_list = handle_datas.handle_data4(testcase_headers)
+
+        # 处理用例variables变量列表
+        testcase_variables = testcase_request.get('test').get('variables')
+        testcase_variables_list = handle_datas.handle_data2(testcase_variables)
+
+        # 处理form表单数据
+        testcase_form_datas = testcase_request_datas.get('data')
+        testcase_form_datas_list = handle_datas.handle_data6(testcase_form_datas)
+
+        # 处理json数据
+        # testcase_json_datas = str(testcase_request_datas.get('json'))
+        testcase_json_datas = json.dumps(testcase_request_datas.get('json'), ensure_ascii=False)
+
+        # 处理extract数据
+        testcase_extract_datas = testcase_request.get('test').get('extract')
+        testcase_extract_datas_list = handle_datas.handle_data3(testcase_extract_datas)
+
+        # 处理parameters数据
+        testcase_parameters_datas = testcase_request.get('test').get('parameters')
+        testcase_parameters_datas_list = handle_datas.handle_data3(testcase_parameters_datas)
+
+        # 处理setupHooks数据
+        testcase_setup_hooks_datas = testcase_request.get('test').get('setup_hooks')
+        testcase_setup_hooks_datas_list = handle_datas.handle_data5(testcase_setup_hooks_datas)
+
+        # 处理teardownHooks数据
+        testcase_teardown_hooks_datas = testcase_request.get('test').get('teardown_hooks')
+        testcase_teardown_hooks_datas_list = handle_datas.handle_data5(testcase_teardown_hooks_datas)
+
+        selected_configure_id = testcase_include.get('config')
+        selected_interface_id = testcase_obj.interface_id
+        selected_project_id = Interfaces.objects.get(id=selected_interface_id).project_id
+        selected_testcase_id = testcase_include.get('testcases')
+
+        datas = {
+            "author": testcase_obj.author,
+            "testcase_name": testcase_obj.name,
+            "selected_configure_id": selected_configure_id,
+            "selected_interface_id": selected_interface_id,
+            "selected_project_id": selected_project_id,
+            "selected_testcase_id": selected_testcase_id,
+
+            "method": testcase_request_datas.get('method'),
+            "url": testcase_request_datas.get('url'),
+            "param": testcase_params_list,
+            "header": testcase_headers_list,
+            "variable": testcase_form_datas_list,  # form表单请求数据
+            "jsonVariable": testcase_json_datas,
+
+            "extract": testcase_extract_datas_list,
+            "validate": testcase_validate_list,
+            "globalVar": testcase_variables_list,  # 变量
+            "parameterized": testcase_parameters_datas_list,
+            "setupHooks": testcase_setup_hooks_datas_list,
+            "teardownHooks": testcase_teardown_hooks_datas_list,
+        }
+        return Response(datas)
